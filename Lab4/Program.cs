f19be3b1764044eba4b4bb090f8af4ec1d1b2e8a
Hanio90 <hanio90@gmail.com>
2018-01-25 20:06:27 -0500

    This is my Lab4

---
+++ Lab4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            long i, number, fact;
+
+                Console.WriteLine("Hello! Please enter an integer between 1 and 10");
+
+                number = int.Parse(Console.ReadLine());
+                fact = number;
+
+                for (i = number - 1; i >= 1; i--)
+                {
+                    fact = fact * i;
+                }
+                Console.WriteLine("Factorial of given Number is : " + fact);
+
+            Console.WriteLine("Continue? Y/N" );
+            string response = Console.ReadLine();
+
+            if (response.ToLower() == "yes" || response.ToLower() == "y" )
+            Main(args);
+
+            else
+                Console.WriteLine("Goodbye");
+
+           // Console.WriteLine(GetFactorial(5));
+
+        }
+
+        /*public static long GetFactorial(int n)
+
+        {
+            if (n == 1)
+                return 1;
+
+            return (n * GetFactorial(n - 1));
+        } */
+    }
+}
